f039b073a28232252015419f8e2b68b4c4a1e090
AnantheshJShet <shetanantheshnlsv@yahoo.in>
2018-03-24 16:25:40 +0530

    Take care of regularization loss

--- main.py
+++ main.py
@@ -98,14 +98,17 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     correct_label = tf.reshape(correct_label, (-1, num_classes))
     
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=correct_label, logits=logits))
+    # Made the below change as per "Tips" mentioned in the Udacity page Project:Semantic Segmentation
+    entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=correct_label, logits=logits))
+    regularization_loss = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+    loss = entropy_loss + regularization_loss
     
     with tf.name_scope("training"):
        optimizer = tf.train.AdamOptimizer()
        global_step = tf.Variable(0, name='global_step', trainable=False)
-       train_op = optimizer.minimize(cross_entropy_loss, global_step=global_step)
+       train_op = optimizer.minimize(loss, global_step=global_step)
        
-    return logits, train_op, cross_entropy_loss
+    return logits, train_op, loss
 tests.test_optimize(optimize)
 
 
