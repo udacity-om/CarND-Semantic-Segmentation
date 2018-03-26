d50dcccc604c92a7fae42650b8f445d7bf38aa92
AnantheshJShet <shetanantheshnlsv@yahoo.in>
2018-03-26 21:44:03 +0530

    final model

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
     
-    epochs = 1
+    epochs = 30
     batches = 8
 
     with tf.Session() as sess:
